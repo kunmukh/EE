0ab2256545e130a0f82e00e81910e6ee87d9cfd8
kunmukh <kunmukh@gmail.com>
2018-08-28 01:57:58 -0500

    Save and Load function working. Trying to get the submit button to work.

--- ASCII_art/ASCII_art/ASCII_art/MainWindow.xaml.cs
+++ ASCII_art/ASCII_art/ASCII_art/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+ 
 
 namespace ASCII_art
 {
@@ -22,7 +26,98 @@ namespace ASCII_art
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
+        public BitmapImage bmi;
+        public WriteableBitmap wbm;
+        public WriteableBitmap wbmSave;
+        public BitmapImage bmiSave;
+        private int winWidth, winHeight;
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";            
+            if (op.ShowDialog() == true)
+            {
+                bmi = new BitmapImage(new Uri(op.FileName));
+                wbm = new WriteableBitmap(bmi);
+                imgPicture.Source = wbm;
+                bmiSave = bmi;
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Displays a SaveFileDialog so the user can save the Image  
+            // assigned to btnSave.  
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();            
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.ShowDialog();           
+
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bmiSave));
+
+            using (var fileStream = new System.IO.FileStream(saveFileDialog1.FileName + ".png", System.IO.FileMode.Create))
+            {
+                encoder.Save(fileStream);
+            }
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            winWidth = (int)imgPicture.Width;
+            winHeight = (int)imgPicture.Height;
+            DrawingVisual vis = new DrawingVisual();
+            DrawingContext dc = vis.RenderOpen();
+            //
+            for (int i = 0; i < wbm.PixelWidth; i++)
+            {
+                for (int j = 0; j < wbm.PixelHeight; j++)
+                {
+                    Color c = new Color();
+                    c = GetPixel(wbm, i, j);
+                    int val = (c.R + c.G + c.B) / 3;                    
+                    dc.DrawText(
+                         new FormattedText("O",
+                         CultureInfo.GetCultureInfo("en-us"),
+                         FlowDirection.LeftToRight,
+                          new Typeface("Verdana"),
+                          1, System.Windows.Media.Brushes.Red),
+                          new System.Windows.Point(i, j));
+                }
+            }
+            
+            //
+            dc.Close();
+            RenderTargetBitmap bmp = new RenderTargetBitmap(wbm.PixelWidth, wbm.PixelHeight, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(vis);
+            imgPicture.Source = bmp;
+        }
+        
+
+        public Color GetPixel(WriteableBitmap wbm, int x, int y)
+        {
+            if (y > wbm.PixelHeight - 1 || x > wbm.PixelWidth - 1)
+                return Color.FromArgb(0, 0, 0, 0);
+            if (y < 0 || x < 0)
+                return Color.FromArgb(0, 0, 0, 0);
+            if (!wbm.Format.Equals(PixelFormats.Bgra32))
+                return Color.FromArgb(0, 0, 0, 0); ;
+            IntPtr buff = wbm.BackBuffer;
+            int Stride = wbm.BackBufferStride;
+            Color c;
+            unsafe
+            {
+                byte* pbuff = (byte*)buff.ToPointer();
+                int loc = y * Stride + x * 4;
+                c = Color.FromArgb(pbuff[loc + 3], pbuff[loc + 2],
+                                       pbuff[loc + 1], pbuff[loc]);
+            }
+            return c;
+        }
+
+
+
     }
 }
