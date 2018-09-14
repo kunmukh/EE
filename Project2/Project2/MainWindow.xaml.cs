122c0396681f48b2e79229f0dcf644d68a16372d
kunmukh <kunmukh@gmail.com>
2018-09-14 02:06:57 -0500

    The plots seems to be working

--- Project2/Project2/MainWindow.xaml.cs
+++ Project2/Project2/MainWindow.xaml.cs
@@ -41,7 +41,12 @@ namespace Project2
         public string fname = "";
         public string fnameOut = @"C:\Users\kunmu\Documents\Kunal\EE\FilteredWav.wav";
         public int filter = 0;
-        private Chart chtSin = new Chart();
+        private Chart chtTime = new Chart();
+        private Chart chtFreq = new Chart();
+        public double timeStart = 0;
+        public double timeEnd = 0;
+        public double freqStart = 0;
+        public double freqEnd = 0;
 
         private void mnuOpenFile_Click(object sender, RoutedEventArgs e)
         {
@@ -56,7 +61,7 @@ namespace Project2
             {
                 fname = op.FileName;
                 waveInformation(fname);
-            }
+            }                        
         }
 
         private void waveInformation(String fileName)
@@ -117,44 +122,99 @@ namespace Project2
             {
                 System.Windows.Forms.Integration.WindowsFormsHost host =
                     new System.Windows.Forms.Integration.WindowsFormsHost();
-                host.Child = chtSin;
+                host.Child = chtTime;
                 // Add the chart to the canvas so it can be displayed.
-                this.cnvChartT.Children.Add(host);
+                this.cnvChartT.Children.Add(host);                
 
             }
+        }
+
+        private void btnPlotTime_Click(object sender, RoutedEventArgs e)
+        {
+            chtTime.ChartAreas.Clear();
+            chtTime.ChartAreas.Add("Default");
+
+            object result = null;
+            matlab.Feval("MLReadWavFile", 2, out result, fname);
+            object[] res = result as object[];
+            double[,] d = (double[,])res[0];
+            fs = (double)res[1];
+
+            double t, y, tIncr;
+
+            // Add a series with some data points.
+            chtTime.Width = 390;
+            chtTime.Height = 151;
+            chtTime.Location = new System.Drawing.Point(0, 0);
+            Series timeSeries = new Series();
+
+            timeSeries.ChartType = SeriesChartType.Line;
+            tIncr = 1;
+            timeStart = Convert.ToDouble(txtbxTimeStart.Text);
+            timeEnd = Convert.ToDouble(txtbxTimeEnd.Text);
+            int rowVal = 0;
+            for (t = timeStart; t < timeEnd; t += tIncr)
+            {
+                y = d[rowVal, 0];
+                rowVal++;
+                timeSeries.Points.AddXY(t, y);
+            }
+            chtTime.Series.Add(timeSeries);
+            chtTime.ChartAreas[0].AxisX.Title = "Time";
+            chtTime.ChartAreas[0].AxisX.LabelStyle.Format = "{0.00}";
+            chtTime.ChartAreas[0].AxisY.Title = "Amplitude";
+            chtTime.ChartAreas[0].AxisY.LabelStyle.Format = "{0.00}";
         }
 
         private void cnvChartF_Loaded(object sender, RoutedEventArgs e)
         {
-            int i = 0;
+            System.Windows.Forms.Integration.WindowsFormsHost host =
+                   new System.Windows.Forms.Integration.WindowsFormsHost();
+            host.Child = chtFreq;
+            // Add the chart to the canvas so it can be displayed.
+            this.cnvChartF.Children.Add(host);
         }
-
-            private void btnPlotTime_Click(object sender, RoutedEventArgs e)
-        {
-            double t, y, tIncr;
-            chtSin.ChartAreas.Add("Default");
-            // Add a series with some data points.
-            chtSin.Width = 377;
-            chtSin.Height = 151;
-            chtSin.Location = new System.Drawing.Point(9, 40);
-            Series sinSeries = new Series();
-            sinSeries.ChartType = SeriesChartType.Line;
-            tIncr = 4 * Math.PI / 500;
-            for (t = 0; t < 4 * Math.PI; t += tIncr)
-            {
-                y = Math.Sin(2 * Math.PI * t);
-                sinSeries.Points.AddXY(t, y);
-            }
-            chtSin.Series.Add(sinSeries);
-            chtSin.ChartAreas[0].AxisX.Title = "Time";
-            chtSin.ChartAreas[0].AxisX.LabelStyle.Format = "{0.00}";
-            chtSin.ChartAreas[0].AxisY.Title = "Volts";
-            chtSin.ChartAreas[0].AxisY.LabelStyle.Format = "{0.00}";
-        }
+            
 
         private void btnPlotFreq_Click(object sender, RoutedEventArgs e)
         {
-            int k = 0;
+            chtFreq.ChartAreas.Clear();
+            chtFreq.ChartAreas.Add("Default");
+
+            object result2 = null;
+            matlab.Feval("MLFreqResp", 2, out result2, fname);
+            object[] res2 = result2 as object[];
+            double[,] fResp = (double[,])res2[0];
+            double[,] freq = (double[,])res2[1];
+
+            double t, x, y, tIncr;
+
+            // Add a series with some data points.
+            chtFreq.Width = 390;
+            chtFreq.Height = 151;
+            chtFreq.Location = new System.Drawing.Point(0, 10);
+            Series timeSeries = new Series();
+
+            timeSeries.ChartType = SeriesChartType.Line;
+
+            tIncr = 500;
+            freqStart = Convert.ToDouble(txtbxFreqStart.Text);
+            freqEnd = fs/2;
+            txtbxFreqEnd.Text = (fs / 2).ToString();
+            int rowVal = 1;
+            for (t = freqStart; t < freqEnd; t += tIncr)
+            {
+                y = fResp[rowVal, 0];
+                x = t;
+                rowVal+=100;
+                timeSeries.Points.AddXY(x, y);
+            }
+
+            chtFreq.Series.Add(timeSeries);
+            chtFreq.ChartAreas[0].AxisX.Title = "Frequency";
+            chtFreq.ChartAreas[0].AxisX.LabelStyle.Format = "{0.00}";
+            chtFreq.ChartAreas[0].AxisY.Title = "Amplitude";
+            chtFreq.ChartAreas[0].AxisY.LabelStyle.Format = "{0.00}";
         }
     }
 }
