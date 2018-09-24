a92e147f56625610c78b31ff07c0242f34989b79
kunmukh <kunmukh@gmail.com>
2018-09-24 10:38:50 -0500

    Project 3 created and started

---
+++ Project3/Project3/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Slot[,] OldGenarray = new Slot[8, 9];
+        private Slot[,] NewGenarray = new Slot[8, 9];
+
+
+    }
+
+    public class Slot
+    {
+        // Auto-implemented readonly property:
+        public bool Empty { get; set; }
+        public bool Static { get; set; }
+
+        public Slot()
+        {
+            Empty = true;
+            Static = true;
+        }
+
+        public void setEmpty()
+        {
+            Empty = false;
+        }
+
+        public bool isEmpty()
+        {
+            return Empty;
+        }
+
+        public bool isStatic()
+        {
+            return Static;
+        }
+
+        public void setStatic()
+        {
+            Static = true;
+        }
+
+    }
+           
+}
