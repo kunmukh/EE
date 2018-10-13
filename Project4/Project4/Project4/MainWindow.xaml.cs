0b2edad30cc3aa40f528b482c07a4748dff800e1
kunmukh <kunmukh@gmail.com>
2018-10-13 18:44:37 -0500

    The class cell and coin as well as global variables created

--- Project4/Project4/Project4/MainWindow.xaml.cs
+++ Project4/Project4/Project4/MainWindow.xaml.cs
@@ -20,9 +20,95 @@ namespace Project4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int numCells = 16;
+        private int numCoin = ((16 - 1) - 1) / 2;
+        private bool isWin = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+    }
+
+    public class Cell
+    {
+        public bool _Last { get; set;}
+        public bool _Empty { get; set;}
+        public Coin _c { get; set;}
+
+        public Cell()
+        {
+            _Last = false;
+            _Empty = false;
+            _c = new Coin(0);
+        }
+
+        public Cell(bool last, bool empty, int coinSelect)
+        {
+            _Last = last;
+            _Empty = empty;
+            _c = new Coin(coinSelect);
+        }
+
+        public void setCoinGold()
+        {
+            _c.setColor(2);
+        }
+
+        public void setCoinBlue()
+        {
+            _c.setColor(1);
+        }
+
+        public void setEmpty(bool status)
+        {
+            _Empty = status;
+        }
+
+        public bool isLast()
+        {
+            return _Last;
+        }
+
+        public void setLast(bool status)
+        {
+            _Last = status;
+        }
+
+        public bool isEmpty()
+        {
+            return _Empty;
+        }
+
+        public Coin getCoin()
+        {
+            return _c;
+        }
+    }
+
+    public class Coin
+    {
+        public int _Color { get; set;}
+
+        public Coin()
+        {
+            _Color = 0;
+        }
+
+        public Coin(int color)
+        {
+            _Color = color;
+        }
+
+        public int getColor()
+        {
+            return _Color;
+        }
+
+        public void setColor(int color)
+        {
+            _Color = color;
+        }
+
     }
 }
